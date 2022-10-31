9b88fcbfab338b2f1db41aff4b183b074d6424c9
prathmesh <prathmeshbavge@gmail.com>
2022-10-31 17:19:16 +0530

    This is add folder commit!

---
+++ file.js
@@ -0,0 +1,18 @@
+console.log("Script is running in the console.");
+
+
+let Participant1 = document.getElementById("groupmemberid1");
+let Participant2 = document.getElementById("groupmemberid2");
+let Participant3 = document.getElementById("groupmemberid3");
+// console.log(Participant1);
+
+
+
+
+
+// document.getElementById("show").addEventListener('click', (ev)=>{
+//     let url = 'this.html';
+    
+// })
+
+
